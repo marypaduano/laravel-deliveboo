bc323315816554bdc329ee210a4a38875ec3f125
marypaduano <maria.paduano@gmail.com>
2023-06-02 13:02:11 +0200

    fix routes

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,65 @@
+<section class="nav-footer">
+    <div class="container">
+        <div class="row d-flex">
+            <div class="col d-flex justify-content-center align-item-center">
+                <figure class="thumb img-fluid flex-shrink-0">
+                    <img src="/images/hamburger-logo.png" alt="">
+                </figure>
+            </div>
+            <div class="col flex-wrap">
+                <ul class="menu">
+                    <li>
+                        <h4>scopri deliveboo</h4>
+                    </li>
+                    <li class="menu_item">
+                        <span>Chi siamo</span>
+                    </li>
+                    <li class="menu_item">
+                        <span>Lavora con noi</span>
+                    </li>
+                    <li class="menu_item">
+                        <span>Diventa nostro partner</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="col flex-wrap">
+                <ul class="menu">
+                    <li>
+                        <h4>note legali</h4>
+                    </li>
+                    <li class="menu_item">
+                        <span>Termini & condizioni</span>
+                    </li>
+                    <li class="menu_item">
+                        <span>Informativa sulla privacy</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="col flex-wrap">
+                <ul class="menu">
+                    <li>
+                        <h4>follow us</h4>
+                    </li>
+                    <li class="menu_item social_icon">
+                        <a href="https://www.facebook.com/" target="_blank" class="icon fb">
+                            <font-awesome-icon :icon="['fab', 'facebook']" />
+                        </a>
+                        <a href="https://twitter.com/" target="_blank" class="icon tw">
+                            <font-awesome-icon :icon="['fab', 'twitter']" />
+                        </a>
+                        <a href="https://www.instagram.com/" target="_blank" class="icon insta">
+                            <font-awesome-icon :icon="['fab', 'instagram']" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="copy">
+    <div class="container text-center">
+        <span>
+            <i>DeliveBoo 2023&copy; created by <strong>Team 6</strong> of Boolean #Class86</i>
+        </span>
+    </div>
+</section>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,63 @@
+<nav class="navbar nav-main navbar-expand-md navbar-light bg-white shadow-sm" style="font-size: 18px;">
+    <div class="container">
+        <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+            <div class="logo_laravel">
+                <img src="/logo-deliveboo1.png" alt="logo" style="width: 100px;">
+            </div>
+            {{-- config('app.name', 'Laravel') --}}
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
+                </li>
+                @auth
+                <li>
+                    <a class="nav-link" href="{{route('dashboard')}}">{{__('Dashboard')}}</a>
+                </li>
+                @endauth
+
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Accedi') }}</a>
+                </li>
+                @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrati') }}</a>
+                </li>
+                @endif
+                @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ url('dashboard') }}">{{__('Dashboard')}}</a>
+                        <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
